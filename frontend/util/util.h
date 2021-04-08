1f2736aaf404d15383db2473675d0e322492c9f8
LFMP <luizfellipe42@hotmail.com>
2021-04-08 14:50:16 -0300

    new organization

--- frontend/util/util.h
+++ frontend/util/util.h
@@ -1,3 +1,6 @@
+#ifndef UTIL_H__
+#define UTIL_H__
+
 #include <assert.h>
 
 typedef char *string;
@@ -10,6 +13,9 @@ void *checked_malloc(int);
 string String(char *);
 
 typedef struct U_boolList_ *U_boolList;
-struct U_boolList_ {bool head; U_boolList tail;};
+struct U_boolList_ {
+  bool head;
+  U_boolList tail;
+};
 U_boolList U_BoolList(bool head, U_boolList tail);
-
+#endif
